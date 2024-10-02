000d8fc4b7d72341510325d770c0b9766ad3214c
mr Anyway <simonstephano328@gmail.com>
2024-10-02 14:56:46 +0300

    Update chanel.js

--- commandes/chanel.js
+++ commandes/chanel.js
@@ -23,7 +23,7 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *MSELA-CHUI-V2* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+   let z ='Salut je m\'appelle *MR-ANYWAY-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
       let d =' developpé par *Thomas*'
       let varmess=z+d
       var img='https://telegra.ph/file/f660abdbefbae32daeb81.jpg'
