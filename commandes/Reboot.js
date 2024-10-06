6cf944045c30b53ba908ddf7081b101c203cb2ea
Kingbegaa <barakachachabegaa@gmail.com>
2024-10-06 12:38:39 +0300

    Update Reboot.js

--- commandes/Reboot.js
+++ commandes/Reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("BMW-MD bot Restarting ⏳");
+    repondre("ANYWAY-MD bot Restarting ⏳");
 
   exec("pm2 restart all");
   
