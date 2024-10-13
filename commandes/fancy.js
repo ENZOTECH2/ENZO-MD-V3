d54de0b829efb780a3f6a4cf53e5b7ee182e8576
mr Anyway <simonstephano328@gmail.com>
2024-10-13 17:26:08 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -1,5 +1,5 @@
 const { zokou } = require("../framework/zokou");
-const fancy = require("../bmw/style");
+const fancy = require("../Anyway/style");
 
 zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 Baraka-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('BARAKA-MD', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 Anyway-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('ANYWAY-MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
