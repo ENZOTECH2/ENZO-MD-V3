23d10f15549e3bf375b420ce2a6a8ec398008761
Popkid <popkidtechs@gmail.com>
2024-10-14 17:59:03 +0300

    ⭕POP KID⭕

--- commandes/Reboot.js
+++ commandes/Reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("ANYWAY-MD bot Restarting ⏳");
+    repondre("POPKID-MD bot Restarting ⚽");
 
   exec("pm2 restart all");
   
