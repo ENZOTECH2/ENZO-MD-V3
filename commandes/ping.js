600e12c86d5def96a4d4c620811c44d5d5b39438
ENZO <aron26296@gmail.com>
2024-10-15 07:05:54 +0300

    ⭕POP KID⭕

--- commandes/ping.js
+++ commandes/ping.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "test", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*⭕𝗣𝗢𝗣𝗞𝗜𝗗 𝗜𝗦 𝗢𝗡𝗟𝗜𝗡𝗘⭕* 🙏 \n\n ' + "𝗣𝗢𝗣𝗞𝗜𝗗 𝗖𝗔𝗡𝗧 𝗦𝗟𝗘𝗘𝗣⏰⭕";
+    let z = '*⭕𝗘𝗡𝗭𝗢 𝗜𝗦 𝗢𝗡𝗟𝗜𝗡𝗘⭕* 🙏 \n\n ' + "𝗘𝗡𝗭𝗢 𝗖𝗔𝗡𝗧 𝗦𝗟𝗘𝗘𝗣⏰⭕";
     let d = '                                                                           𝗛𝗘𝗔𝗟𝗧𝗛 𝗦𝗧𝗔𝗧𝗨𝗦✨';
     let varmess = z + d;
     var mp4 = 'https://telegra.ph/file/ce58cf8c538b1496fda33.mp4';
