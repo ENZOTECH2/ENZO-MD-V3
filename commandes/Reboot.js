74c8272b7bff0ceb29cf425520c6bba4f7e79f3c
Popkid <popkidtechs@gmail.com>
2024-10-18 23:14:42 +0300

    ⭕POP KID⭕

--- commandes/Reboot.js
+++ commandes/Reboot.js
@@ -6,7 +6,7 @@ const {zokou}=require("../framework/zokou")
 
 
 
-zokou({nomCom:"restart",categorie:"Mods",reaction:"📴"},async(dest,z,com)=>{
+zokou({nomCom:"restart",categorie:"Mods",reaction:"Ⓜ️"},async(dest,z,com)=>{
 
 
   
